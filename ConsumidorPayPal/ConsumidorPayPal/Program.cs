3293b27e5cc71dec79bd97c638eba6d1b07a2385
David Calle <davidcalle9430@gmail.com>
2016-05-14 23:35:00 -0500

    Servicio web y consumidor web simple

---
+++ ConsumidorPayPal/ConsumidorPayPal/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsumidorPayPal.PaypalWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumidorPayPal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client cliente = new Service1Client();
+            Console.WriteLine( cliente.GetData( 5 ) );
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Paypal/Paypal/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Paypal.Models
+{
+    public class Usuario
+    {
+        public string NombreUsuario { get; set; }
+        public string Contrasenia { get; set; }
+        public double Saldo { get; set; }
+    }
+}
